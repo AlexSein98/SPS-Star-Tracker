Small gravity model update + fixed gradient descent SPS algorithm

SPS/SPS_gradient.py
@@ -125,7 +125,7 @@ def estimate_position(truthDataPath: str, estDataPath: str, latLonDataPath: str)
         # Gradient method:
         i: int = 0
 
-        tol: float = 10.0  # m
+        tol: float = 1.0  # m
         dr: np.ndarray[float] = np.zeros(3)
         dr_prev: np.ndarray[float] = 2.0 * tol * np.ones(3)
 
@@ -144,7 +144,7 @@ def estimate_position(truthDataPath: str, estDataPath: str, latLonDataPath: str)
             G = sampler.InterpolateGradientGrid(lat, lon, Gxx, Gyy, Gzz, Gxy, Gxz, Gyz)
             G_inv = np.linalg.inv(G)
 
-            g_sensorFrame: np.ndarray[float] = np.array([-moon.mu / ((moon.radius + alt) ** 2), 0.0, 0.0])
+            g_sensorFrame: np.ndarray[float] = np.array([-np.linalg.norm(g), 0.0, 0.0])
             g_est = T_i_b @ T_i_c.T @ T_g_c @ g_sensorFrame + np.cross(Omega, np.cross(Omega, r_hat_i))
             dg = g - g_est
 
@@ -153,7 +153,7 @@ def estimate_position(truthDataPath: str, estDataPath: str, latLonDataPath: str)
             # print(f'g_est = {g_est}')
             
             dr = G_inv @ dg
-            r_hat_i_plus_1 = r_hat_i - 0.15 * dr
+            r_hat_i_plus_1 = r_hat_i - dr
 
             lat, lon, _ = r_to_latlonalt(r_hat_i_plus_1, moon.radius)
             alt = SampleDEM_LatLon(dem, lat, lon)

SPS/gravity.py
@@ -37,6 +37,7 @@ class GravSampler:
         self.Cilm[0, 0, 0] = 1.0  # add spherical component of gravity
     
     def SampleAcceleration(self, lat: float, lon: float, r: float, maxDegree: float, 
+                           overrideSphericalHarmonics: bool = False, 
                            includeThirdBody: bool = False, et: float = 0.0) -> np.ndarray[float]:
         """
         Latitude and longitude must be in degrees!
@@ -48,12 +49,15 @@ class GravSampler:
         T = latlon_to_T(lat, lon)
         pos_surface = r * T[:, 0]
         g_pcpf = T @ MakeGravGridPoint(self.Cilm, mu, R, r, lat, lon, maxDegree, omega)
+        
+        # Choose to override spherical harmonics with just spherical gravity (or not):
+        if overrideSphericalHarmonics:
+            g_pcpf = -moon.mu * pos_surface / (r ** 3)
 
-        # Third-body perturbations:
+        # Choose to do third-body perturbations (or not):
         if not includeThirdBody:
             return g_pcpf
-
-        # g_pcpf = -moon.mu * pos_surface / (r ** 3)  # Override spherical harmonics with just spherical gravity
+        
         planetIDs = [10, 199, 299, 399, 499, 599, 699, 799, 899]  # no Pluto hehe >:)
         T_moon = spice.pxform("J2000", "MOON_PA", et)
         for id in planetIDs:
@@ -63,7 +67,7 @@ class GravSampler:
             pos_m = 1000.0 * pos_km
 
             muResult = spice.bodvrd(name, "GM", 1)
-            mu: float = muResult[1][0]
+            mu: float = muResult[1][0] * 1e9
             r_sat_body: np.ndarray[float] = pos_m - T_moon.T @ pos_surface
             g_inertial = mu * (r_sat_body / (np.linalg.norm(r_sat_body) ** 3) - pos_m / (np.linalg.norm(pos_m) ** 3))
             g_pcpf += T_moon @ g_inertial

SPS/gravity_var.py
@@ -33,7 +33,7 @@ from SPS.gravity import *
 
 
 def angle_between_gravities(real_grav, sphere_grav):
-  return np.arccos(np.dot(normalize(real_grav), normalize(sphere_grav)))
+  return arccos_safe(np.dot(normalize(real_grav), normalize(sphere_grav)))
 
 
 
@@ -119,16 +119,20 @@ if __name__ == "__main__":
       angle_matrix.append([])
       for j in range(len(lats[i])):
         sphere_grav = -normalize(latlon_to_T(lats[i][j], lons[i][j]).T[0])
-        true_grav = sampler.SampleAcceleration(lats[i][j], lons[i][j], moon.radius, maxDegree, True, etNow)
+        true_grav = sampler.SampleAcceleration(lats[i][j], lons[i][j], moon.radius, maxDegree,
+                                               overrideSphericalHarmonics=True, includeThirdBody=True, et=etNow)
         _angle = angle_between_gravities(true_grav, sphere_grav)
         angle_matrix[i].append(_angle)
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
+    print(f'Max deviation = {np.max(deg_to_arcsec(np.rad2deg(np.asarray(angle_matrix))))}"')
+
     plt.imshow(deg_to_arcsec(np.rad2deg(np.asarray(angle_matrix))), cmap='RdYlGn_r', aspect='equal', extent = [-180,180,-90,90])
     plt.colorbar(label='Gravity Vector Difference (arcsec)')
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
-    plt.title("Error Between Spherical Harmonic and Pure Spherical Gravity Models")
+    # plt.title("Error Between Spherical Harmonic and Pure Spherical Gravity Models")
+    plt.title("Error Between 3rd Body Perturbation and Pure Spherical Gravity Models")
     plt.show()

py_src/star/python/transformations.py
@@ -314,13 +314,32 @@ class Quaternion:
 
 if __name__ == "__main__":
     os.system('cls')
+    np.set_printoptions(suppress=True)
+    testMode = 1
 
-    r = np.array([2193075.113333, 743921.623579, -2485679.376025])
-    R_mars = 3396190.0
-    lat, lon, alt = r_to_latlonalt(r, R_mars)
-    T = latlon_to_T(lat, lon) @ T2(np.pi / 2.0)
-    q: Quaternion = Quaternion.FromMatrix(T)
+    # For Space Teams University Competition 3
+    if testMode == 0:
+        r = np.array([2193075.113333, 743921.623579, -2485679.376025])
+        R_mars = 3396190.0
+        lat, lon, alt = r_to_latlonalt(r, R_mars)
+        T = latlon_to_T(lat, lon) @ T2(np.pi / 2.0)
+        q: Quaternion = Quaternion.FromMatrix(T)
 
-    print(f'lat = {round(lat, 6)}, lon = {round(lon, 6)}\n')
-    print(f'T = {T}\n')
-    print(f'q = [{round(q.w, 6)}, {round(q.x, 6)}, {round(q.y, 6)}, {round(q.z, 6)}]')
+        print(f'lat = {round(lat, 6)}, lon = {round(lon, 6)}\n')
+        print(f'T = {T}\n')
+        print(f'q = [w={round(q.w, 6)}, x={round(q.x, 6)}, y={round(q.y, 6)}, z={round(q.z, 6)}]')
+    # For Phobos alignment
+    elif testMode == 1:
+        import spiceypy as spice
+        home = "./py_src/star/"
+        spice.furnsh(home + "data/metakernel.txt")
+        tNow = '2025 July 4, 00:00:00 UTC'
+        etNow = spice.str2et(tNow)
+
+        marsLoc, _ = spice.spkpos("MARS", etNow, "IAU_PHOBOS", "NONE", "PHOBOS")
+        marsRot = spice.pxform("IAU_MARS", "IAU_PHOBOS", etNow)
+        q = Quaternion.FromMatrix(marsRot)
+
+        print(f'Mars location = {marsLoc * 1000.0}')
+        print(f'Mars rotation = [w={round(q.w, 6)}, x={round(q.x, 6)}, y={round(q.y, 6)}, z={round(q.z, 6)}]')
+